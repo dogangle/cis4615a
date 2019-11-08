c8daaf48748db0e1116ceaeac22137aff0ee4857
dogangle <51759064+dogangle@users.noreply.github.com>
2019-11-07 20:52:19 -0500

    Add files via upload

--- R49_MSC01_J.java
+++ R49_MSC01_J.java
@@ -4,6 +4,7 @@ Execution: java R49_MSC01_J
 
  - Initial commit of non-compliant code
  - testing
+ - fixed code to be compliant
  
 This code is non-compliant because it performs an infinite empty loop.
  
@@ -13,14 +14,17 @@ This code is non-compliant because it performs an infinite empty loop.
 public class R49_MSC01_J {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		R49_MSC01_J test = new  R49_MSC01_J();
+		System.out.println("This compliant code utilizes an empty infinite loop by properly invoking Thread.sleep() within the loop");
 		test.nop();
 	}
 	
-	public void nop() {
+	public void nop() throws InterruptedException {
+		int duration = 10000;
 		while(true) {
+			Thread.sleep(duration);
 		}
 	}
 	
